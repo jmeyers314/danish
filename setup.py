e0baf18e95531e7ea52fae4c522850edd206cd74
Josh Meyers <jmeyers314@gmail.com>
2024-02-05 20:33:06 -0800

    /->.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     description="Geometric donut engine",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    packages=find_packages(exclude=['danish/data', 'danish/data/*']),
+    packages=find_packages(exclude=['danish.data', 'danish.data.*']),
     package_data={
         "danish": ["data/*"],
     },
