55700a28e34cb69190d8de87b5584679e8ab421b
Josh Meyers <jmeyers314@gmail.com>
2025-09-08 15:49:55 -0700

    Add DZ Mode Fitter

--- danish/fitter.py
+++ danish/fitter.py
@@ -285,7 +285,7 @@ class MultiDonutModel:
     dz_terms : sequence of (int, int)
         Which double Zernike coefficients to include in the fit.
     thxs, thys : float
-        Field angle in radians.
+        Field angles in radians.
     npix : int
         Number of pixels along image edge.  Must be odd.
     seed : int
@@ -538,6 +538,294 @@ class MultiDonutModel:
         out[:, 2*nstar] = (chi1-chi0)/0.001
 
         # DZ terms
+        for i in range(2*nstar+1, len(params)):
+            params1 = np.array(params)
+            params1[i] += 1e-8
+            chi1 = self.chi(params1, data, vars)
+            out[:, i] = (chi1-chi0)/1e-8
+
+        return out
+
+
+class DZModeDonutModel:
+    """ Model multiple donuts using modes of double Zernike coefficients.
+
+    Parameters
+    ----------
+    factory : DonutFactory
+    dz_ref : DoubleZernike
+        Double Zernike coefficients to use for constructing Single Zernike
+        reference coefficients to use for each modeled donut.  Either this kwarg or `z_refs` must be set.
+    z_refs : array of float
+        Single Zernike reference coefficients for each donut.  First dimension
+        is donut, second dimension is pupil Zernike coefficient.
+    field_radius : float
+        Field radius in radians.  Ignored if dz_ref is provided.
+    sensitivity : array of float
+        Matrix of shape (nmode, kmax+1, jmax+1) giving the linear combination of
+        double Zernike coefficients for each mode.
+    thxs, thys : float
+        Field angles in radians.
+    npix : int
+        Number of pixels along image edge.  Must be odd.
+    seed : int
+        Random seed for use when creating noisy donut images with this class.
+    """
+    def __init__(
+        self,
+        factory, *,
+        dz_ref=None,
+        z_refs=None,
+        field_radius=None,
+        sensitivity=None,
+        thxs=None, thys=None,
+        npix=181,
+        seed=5772156
+    ):
+        if sensitivity is None:
+            raise ValueError("Must provide sensitivity")
+        self.sensitivity = sensitivity
+        self.nmode = sensitivity.shape[0]
+        self.kmax = sensitivity.shape[1]-1
+        self.jmax = sensitivity.shape[2]-1
+
+        self.factory = factory
+        # arcseconds per pixel
+        # As with SingleDonutModel, we only use the scale here for the
+        # atmospheric part, and the kernel is isotropic in pixel coordinates,
+        # ignoring distortion.
+        self.sky_scale = (
+            3600*np.rad2deg(1/factory.focal_length)*factory.pixel_scale
+        )
+
+        if dz_ref is None and z_refs is None:
+            raise ValueError("Must provide dz_ref or z_refs")
+        if dz_ref is not None and z_refs is not None:
+            raise ValueError("Cannot provide both dz_ref and z_refs")
+        if z_refs is None:
+            z_refs = dz_ref(thxs, thys)
+        self.z_refs = z_refs
+        if field_radius is None:
+            if dz_ref is None:
+                raise ValueError("Must provide dz_ref or field_radius")
+            field_radius = dz_ref.field_radius
+        self.field_radius = field_radius
+        self.thxs = thxs
+        self.thys = thys
+        self.npix = npix
+        self.no2 = (npix-1)//2
+        self.gsrng = galsim.BaseDeviate(seed)
+        self.nstar = len(thxs)
+
+    @lru_cache(maxsize=1000)
+    def _atm1(self, dx, dy, fwhm):
+        obj = galsim.Kolmogorov(fwhm=fwhm).shift(dx, dy)
+        img = obj.drawImage(nx=self.no2, ny=self.no2, scale=self.sky_scale)
+        return img.array
+
+    @lru_cache(maxsize=1000)
+    def _opt1(self, aberrations, thx, thy):
+        return self.factory.image(
+            aberrations=tuple(aberrations), thx=thx, thy=thy, npix=self.npix
+        )
+
+    def _model1(
+        self,
+        dx, dy, fwhm, aberrations,
+        thx, thy, *,
+        sky_level=None, flux=None
+    ):
+        atm = self._atm1(dx, dy, fwhm)
+        opt = self._opt1(tuple(aberrations), thx, thy)
+        arr = convolve(opt, atm)
+        img = galsim.Image(arr)  # Does this make a copy?
+        if flux is not None:
+            img.array[:] *= flux/np.sum(img.array)
+        if sky_level is not None:
+            pn = galsim.PoissonNoise(self.gsrng, sky_level=sky_level)
+            img.addNoise(pn)
+        return img.array
+
+    def model(
+        self, dxs, dys, fwhm, mode_fit, *, sky_levels=None, fluxes=None
+    ):
+        """Compute model for all donuts.
+
+        Parameters
+        ----------
+        dxs, dys : float
+            Offsets in pixels(?)
+        fwhm : float
+            Full width half maximum of Kolmogorov kernel.
+        mode_fit : sequence of float
+            Mode perturbations.
+        sky_levels : sequence of float
+            Sky levels to use when adding Poisson noise to images.
+        fluxes : sequence of float
+            Flux levels at which to set images.
+
+        Returns
+        -------
+        imgs : array of float.  Shape: (nstar, npix, npix)
+            Model images.
+        """
+        nstar = self.nstar
+        npix = self.npix
+        if sky_levels is None:
+            sky_levels = [None]*nstar
+        if fluxes is None:
+            fluxes = [None]*nstar
+
+        out = np.empty((nstar, npix, npix))
+        dzarr = np.einsum(
+            "i,ikj->kj",
+            mode_fit,
+            self.sensitivity
+        )
+        dz = DoubleZernike(dzarr, field_radius=self.field_radius)
+
+        for i, (thx, thy) in enumerate(zip(self.thxs, self.thys)):
+            aberrations = np.array(self.z_refs[i])
+            z_fit = dz(thx, thy)
+            aberrations[:len(z_fit)] += z_fit
+            out[i] = self._model1(
+                dxs[i], dys[i],
+                fwhm,
+                aberrations,
+                thx, thy,
+                sky_level=sky_levels[i], flux=fluxes[i]
+            )
+        return out
+
+    def unpack_params(self, params):
+        """Utility method to unpack params list
+
+        Parameters
+        ----------
+        params : list
+            Parameter list to unpack
+
+        Returns
+        -------
+        dxs, dys : list
+            Model position offsets.
+        fwhm : float
+            Model FWHM.
+        mode_fit : list
+            Model mode coefficients.
+        """
+        dxs = params[:self.nstar]
+        dys = params[self.nstar:2*self.nstar]
+        fwhm = params[2*self.nstar]
+        mode_fit = params[2*self.nstar+1:]
+        return dxs, dys, fwhm, mode_fit
+
+    def chi(
+        self, params, data, vars
+    ):
+        """Compute chi = (data - model)/error.
+
+        The error is modeled as sqrt(model + var) for each donut.
+
+        Parameters
+        ----------
+        params : sequence of float
+            Order is: (dx, dy, fwhm, *mode_fit)
+        data : array of float.  Shape: (nstar, npix, npix)
+            Images against which to compute chi.
+        vars : sequence of array (npix, npix) or sequence of float
+            Variances of the sky only.  Do not include Poisson contribution of
+            the signal, as this will be added from the current model.
+
+        Returns
+        -------
+        chi : array of float
+            Flattened array of chi residuals.
+        """
+        dxs, dys, fwhm, mode_fit = self.unpack_params(params)
+        mods = self.model(dxs, dys, fwhm, mode_fit)
+        chis = np.empty((self.nstar, self.npix, self.npix))
+        for i, (mod, datum) in enumerate(zip(mods, data)):
+            mod *= np.sum(datum)/np.sum(mod)
+            chis[i] = (datum-mod)/np.sqrt(vars[i] + mod)
+        return chis.ravel()
+
+    def _chi1(self, dx, dy, fwhm, aberrations, thx, thy, datum, var):
+        mod1 = self._model1(dx, dy, fwhm, aberrations, thx, thy)
+        mod1 *= np.sum(datum)/np.sum(mod1)
+        return ((datum-mod1)/np.sqrt(var+mod1)).ravel()
+
+    def jac(
+        self, params, data, vars
+    ):
+        """Compute jacobian d(chi)/d(param).
+
+        Parameters
+        ----------
+        params : sequence of float
+            Order is: (dx, dy, fwhm, *mode_fit)
+        data : array of float.  Shape: (nstar, npix, npix)
+            Image against which to compute chi.
+        vars : sequence of array (npix, npix) or sequence of float
+            Variances of the sky only.  Do not include Poisson contribution of
+            the signal, as this will be added from the current model.
+
+        Returns
+        -------
+        jac : array of float
+            Jacobian array d(chi)/d(param).  First dimenison is chi, second
+            dimension is param.
+        """
+        nstar = self.nstar
+        npix = self.npix
+
+        out = np.zeros((nstar*npix**2, len(params)))
+        dxs, dys, fwhm, mode_fit = self.unpack_params(params)
+
+        dzarr = np.einsum(
+            "i,ikj->kj",
+            mode_fit,
+            self.sensitivity
+        )
+        dz = DoubleZernike(dzarr, field_radius=self.field_radius)
+
+        chi0 = np.zeros(nstar*npix**2)
+
+        # Star dx, dy terms are sparse
+        for i in range(self.nstar):
+            thx, thy = self.thxs[i], self.thys[i]
+            aberrations = np.array(self.z_refs[i])
+            z_fit = dz(thx, thy)
+            aberrations[:len(z_fit)] += z_fit
+            s = slice(i*npix**2, (i+1)*npix**2)
+
+            c0 = self._chi1(
+                dxs[i], dys[i], fwhm,
+                aberrations,
+                thx, thy, data[i], vars[i]
+            )
+            cx = self._chi1(
+                dxs[i]+0.01, dys[i], fwhm,
+                aberrations,
+                thx, thy, data[i], vars[i]
+            )
+            cy = self._chi1(
+                dxs[i], dys[i]+0.01, fwhm,
+                aberrations,
+                thx, thy, data[i], vars[i]
+            )
+
+            out[s, i] = (cx-c0)/0.01
+            out[s, i+nstar] = (cy-c0)/0.01
+            chi0[s] = c0
+
+        # FWHM
+        params1 = np.array(params)
+        params1[2*nstar] += 0.001
+        chi1 = self.chi(params1, data, vars)
+        out[:, 2*nstar] = (chi1-chi0)/0.001
+
+        # Mode terms
         for i in range(2*nstar+1, len(params)):
             params1 = np.array(params)
             params1[i] += 1e-8
